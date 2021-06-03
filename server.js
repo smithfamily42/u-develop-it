ed3ef055cc30eeecf7a73d87ecf17137564ac234
Brad Smith <smithfamily42@gmail.com>
2021-06-03 16:42:01 -0700

    adding first query

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,22 @@ const app = express();
 //middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Capt4Life@23',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+  
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 app.get('/', (req, res) => {
     res.json({

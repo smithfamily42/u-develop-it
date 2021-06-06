3d81bc300bdc461315e65d9cf365ccd141f799dc
Brad Smith <smithfamily42@gmail.com>
2021-06-06 00:30:11 -0700

    broke out the routes into individual files

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Capt4Life@23',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+  module.exports = db;
+  
